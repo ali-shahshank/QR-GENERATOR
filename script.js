aa862cf26ea801c8f6b74ea35e86986d883192ad
ali-shahshank <74662711+ali-shahshank@users.noreply.github.com>
2025-01-12 19:17:40 +0500

    QR CODE GENERATOR

---
+++ script.js
@@ -0,0 +1,17 @@
+let qrText = document.getElementById("qrText");
+let qrImg = document.getElementById("qrImg");
+let imgBox = document.getElementById("imgBox");
+
+function generateQR() {
+  if (qrText.value.trim() !== "") {
+    qrImg.src =
+      "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" +
+      qrText.value;
+    imgBox.classList.add("show-img");
+  } else {
+    qrText.classList.add("error");
+    setTimeout(() => {
+      qrText.classList.remove("error");
+    }, 1000);
+  }
+}
